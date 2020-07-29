0f0357a912ac9efe8185ac9af23a2e7003d76f2a
Steve Lanier <stevekentlanier@gmail.com>
2020-07-29 16:31:58 -0700

    added dummy user data to UserTable. Minor structure changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import UserTable from './tables/UserTables'
+import React, { useState } from 'react'
+import UserTable from './tables/UserTable'
 
 const App = () => {
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Ben', username: 'benisphere' },
+  ]
+
+  const [users, setUsers] = useState(usersData)
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
@@ -11,11 +19,11 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable />
+          <UserTable users={users} />
         </div>
       </div>
     </div>
   )
 }
 
-export default App;
+export default App
